f8c7a5c9d6a5fb2d14076516901035bfda1fe881
Sasacoriror <43811709+Sasacoriror@users.noreply.github.com>
2023-01-06 22:44:24 +0100

    Nå kan mannhente ut data, testet med postman, begynner arbeid med React. Har ikke tatt med application properties

--- src/main/java/com/example/studentregister/Controller/StudentController.java
+++ src/main/java/com/example/studentregister/Controller/StudentController.java
@@ -3,9 +3,12 @@ package com.example.studentregister.Controller;
 import com.example.studentregister.Model.Student;
 import com.example.studentregister.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -16,5 +19,10 @@ public class StudentController {
     @PostMapping("/student")
     public Student nyStrudent(@RequestBody Student nyStudent){
         return studentRepository.save(nyStudent);
+    }
+
+    @GetMapping("/alleStudenter")
+    public List<Student> hentAlle(){
+        return studentRepository.findAll();
     }
 }
